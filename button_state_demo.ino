b493a7c901b740b7e455779dbcfdcb0bb2e26e33
kcl1s <kcl@dmv.com>
2018-02-26 10:06:10 -0500

    Add files via upload

---
+++ button_state_demo.ino
@@ -0,0 +1,21 @@
+bool buttonState = 0;  // Variable for reading the pushbutton status
+bool oldButtonState = 1;  //  Variable for old button state
+const int buttonPin = 5;          // button pin
+
+void setup() {
+   pinMode(buttonPin, INPUT_PULLUP); 
+}
+
+void loop() {
+  buttonState = digitalRead(buttonPin);
+  if ((buttonState = LOW) && (oldButtonState = HIGH)) {   //We have a new button press
+    //
+    //Put button press code here
+    //
+    oldButtonState = LOW;
+  }
+  else if ((buttonState = HIGH) && (oldButtonState = LOW)) {  //We have a button release
+    oldButtonState = HIGH;
+  }
+  delay(50);    //for button debounce
+}
