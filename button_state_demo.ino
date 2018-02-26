85fb33604aed2e37802248b00969840742bba5ef
kcl1s <kcl@dmv.com>
2018-02-26 10:19:54 -0500

    Update button_state_demo.ino

--- button_state_demo.ino
+++ button_state_demo.ino
@@ -1,21 +1,22 @@
-bool buttonState = 0;  // Variable for reading the pushbutton status
-bool oldButtonState = 1;  //  Variable for old button state
+bool buttonState = LOW;  // Variable for reading the pushbutton status
+bool oldButtonState = HIGH;  //  Variable for old button state
 const int buttonPin = 5;          // button pin
 
 void setup() {
-   pinMode(buttonPin, INPUT_PULLUP); 
+   pinMode(buttonPin, INPUT_PULLUP);      //Easier wiring Pressed = LOW
+   Serial.begin(9600);
 }
 
 void loop() {
-  buttonState = digitalRead(buttonPin);
+  buttonState = digitalRead(buttonPin);                  //Read current button state
   if ((buttonState = LOW) && (oldButtonState = HIGH)) {   //We have a new button press
-    //
-    //Put button press code here
-    //
-    oldButtonState = LOW;
+    //press code here
+    Serial.println("Pressed");
+     
+    oldButtonState = LOW;     //update old state
   }
   else if ((buttonState = HIGH) && (oldButtonState = LOW)) {  //We have a button release
-    oldButtonState = HIGH;
+    oldButtonState = HIGH;    //update old state
   }
   delay(50);    //for button debounce
 }
